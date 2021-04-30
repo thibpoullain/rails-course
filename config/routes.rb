c8b3eca122fd7985cac9276f3485e1b503d69167
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-04-30 10:43:39 +0200

    Add API health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/healthtest", to: proc { [200, {}, ["ok"]] }
 end
