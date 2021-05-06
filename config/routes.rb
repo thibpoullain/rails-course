5ce5a533473e63c2c54a74d4fc089dcbc50ee965
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-05-06 08:32:19 +0200

    Nest Post in author + Auth access to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   devise_for :authors
-  resources :posts
   get "/healthtest", to: proc { [200, {}, ["ok"]] }
+
+  scope module: 'authors' do
+    resources :posts
+  end
+
 end
