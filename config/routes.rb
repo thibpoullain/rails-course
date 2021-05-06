6d9e23d4bd1c582f4f417db3a3a8a9e50e468306
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-05-06 08:17:32 +0200

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "/healthtest", to: proc { [200, {}, ["ok"]] }
+  root to: 'posts#index'
+  devise_for :authors
   resources :posts
+  get "/healthtest", to: proc { [200, {}, ["ok"]] }
 end
