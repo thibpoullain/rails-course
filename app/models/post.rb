9d034900891de8394fabfc3b45834ae3437a0680
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-05-07 08:39:26 +0200

    Add validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author
+
+  validates :titre, presence: true
+  validates :description, presence: true
+  validates :published, presence: true
+  validates :published_at, presence: true
+  validates :author, presence: true
 end
